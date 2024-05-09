a9c021f947fe779ec87d1f5f96cee242971349af
michy0902 <michelle.kolawole13@gmail.com>
2024-05-09 22:09:26 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# michys-portfolio-3
---
+++ script.js
@@ -0,0 +1,17 @@
+function redirect(){
+    window.location.href = "about.html";
+}
+function back(){
+    window.location.href = "index.html";
+}
+function next(){
+    window.location.href = "projects.html";
+}
+
+
+var typed = new Typed(".auto-type",{
+    strings: ["michelle kolawole", "a software developer","a monster"],
+    typeSpeed: 90,
+    backSpeed: 90,
+    loop: true
+})
